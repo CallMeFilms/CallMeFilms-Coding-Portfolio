44bb6dbbeef4b962c41b1d7edc0f15f2f32429cb
Lucas Eastman <lucas546alexander@gmail.com>
2018-01-14 16:04:01 -0700

    Added scripting; Altered CSS styles and keyframe rules;
    Added scripting for scoll animations via CSS; Added and altered styles for the content of the index along with keyframes for scroll animations for the nav bar;

--- events.js
+++ events.js
@@ -2,14 +2,26 @@ window.onscroll = function() {changeNav()};
 
 function changeNav() {
   var anchs = document.getElementById('headerNav').getElementsByTagName('a');
-  if(document.body.scrollTop > window.innerHeight || document.documentElement.scrollTop > window.innerHeight) {
+  if (document.body.scrollTop > (window.innerHeight * 2 - 100) || document.documentElement.scrollTop > (window.innerHeight * 2 - 100)) {
     for (var i = 0; i < anchs.length; i++) {
-      anchs[i].className = 'blueNav';
+      if(anchs[i].className == 'orangeToBlueNav' || anchs[i].className == 'brightOrangeToBlueNav') {
+        anchs[i].className = 'blueToBrightOrangeNav';
+      }
     }
-  } else if (document.body.scrollTop < window.innerHeight || document.documentElement.scrollTop < window.innerHeight) {
+  } else if(document.body.scrollTop > (window.innerHeight - 100) || document.documentElement.scrollTop > (window.innerHeight - 100)) {
     for (var i = 0; i < anchs.length; i++) {
-      if(anchs[i].className == 'blueNav') {
-      anchs[i].className = 'orangeNav';
+      if(anchs[i].className == 'blueToOrangeNav') {
+        anchs[i].className = 'orangeToBlueNav';
+      } else if(anchs[i].className == 'blueToBrightOrangeNav') {
+        anchs[i].className = 'brightOrangeToBlueNav';
+      } else {
+        anchs[i].className = 'orangeToBlueNav';
+      }
+    }
+  } else if(document.body.scrollTop < window.innerHeight || document.documentElement.scrollTop < window.innerHeight) {
+    for (var i = 0; i < anchs.length; i++) {
+      if(anchs[i].className == 'orangeToBlueNav' || anchs[i].className == 'brightOrangeToBlueNav') {
+      anchs[i].className = 'blueToOrangeNav';
       }
     }
   }
